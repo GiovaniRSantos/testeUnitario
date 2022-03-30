daca620892cf1f6abd9fce5c07105fa4a9c1a56c
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-03-30 13:20:34 -0300

    class MainTest

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,31 @@
+fun contXO(str: String): Boolean {
+    val strLower = str.lowercase()
+
+    var i = 0
+    var countX = 0
+    var countO = 0
+    while (i < strLower.length) {
+        if (strLower[i] == 'x') {
+            countX++
+        } else if (strLower[i] == 'o') {
+            countO++
+        }
+        i++
+    }
+    return countO == countX && countO != 0
+}
+
+fun methodTest(): Boolean {
+    return true
+}
+
+fun TestException() {
+    val str: String? = null
+    str!!.length == 0
+
+}
+
+fun main() {
+    contXO("xxoo")
+    contXO("xxxo")
+}
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.*
+
+class MainTest {
+
+    @Test
+    @DisplayName("Test method testCountXO")
+    fun testCountXO() {
+        Assertions.assertTrue(contXO("xxoo"))
+        Assertions.assertTrue(contXO("xxoO"))
+        Assertions.assertTrue(contXO("xXoo"))
+        Assertions.assertFalse(contXO("xxxo"))
+        Assertions.assertFalse(contXO("xxXo"))
+    }
+
+    @Disabled
+    @DisplayName("Disabled Test")
+    fun disabled() {
+        fail("Não posso terminar os testes sem esse metodo")
+    }
+
+    @Test
+    @DisplayName("Test AssumeTrue")
+    fun assumption() {
+        Assumptions.assumeTrue(contXO("xxoo"))
+        Assertions.assertTrue(methodTest())
+    }
+
+    @Test
+    @DisplayName("Test Exception")
+    fun testException() {
+        assertThrows<java.lang.NullPointerException> {
+            TestException()
+        }
+    }
+
+    @Test
+    @DisplayName("Test fail")
+    fun failTest() {
+        fail("Não posso terminar os testes sem esse metodo")
+    }
+
+}
